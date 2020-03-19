8ba27390c5d2bda8cc344ef607fe72c653143205
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-03-19 09:20:46 +0100

    create navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,117 +1,60 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
     <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
+      <v-navigation-drawer
+        app
+        fill-height
+        floating
+        dark
+      >
+        <v-list
+          dense
+          nav
+          class="py-0"
+        >
+          <v-list-item two-line class="px-0">
+            <v-list-item-content>
+              <v-list-item-title>Chuuuuut</v-list-item-title>
+              <v-list-item-subtitle>Accès pour Ynov</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider v-if="!isLogged"></v-divider>
+          <v-list-item v-if="!isLogged" class="pa-0">
+            <v-list-item-content>
+              <v-row>
+                <v-col cols="12">
+                  <v-text-field v-model="email" label="Email" outlined dense hide-details/>
+                </v-col>
+                <v-col cols="12">
+                  <v-text-field label="Password" v-model="password" outlined dense hide-details/>
+                </v-col>
+                <v-col cols="6" >
+                  <v-btn small outlined>Inscription</v-btn>
+                </v-col>
+                <v-col cols="6" >
+                  <v-btn small outlined>connexion</v-btn>
+                </v-col>
+              </v-row>
+            </v-list-item-content>
+          </v-list-item>
+          <v-divider></v-divider>
+
+        </v-list>
+      </v-navigation-drawer>
+      <nuxt />
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+  export default {
+    data () {
+      return {
+        email: '',
+        password: '',
+        isLogged: false,
+        logInToggle: false,
+      }
+    },
   }
-}
 </script>
