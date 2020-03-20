48385a502b44e04994755ca0796ab7ea970aa96d
AlexisCapdepon <capdepon.alexis@gmail.com>
2020-03-20 18:24:20 +0100

    add canvas draw

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,9 +38,30 @@
             </v-list-item-content>
           </v-list-item>
           <v-divider></v-divider>
-
+          <v-list-item v-if="!isLogged" class="px-0">
+            <v-list-item-content>
+              <v-list-item-title><h1>Inscription</h1></v-list-item-title>
+              <v-row dense>
+                <v-col cols="9">
+                  <v-text-field v-model="email" label="Email" outlined dense hide-details/>
+                </v-col>
+                <v-col cols="9">
+                  <v-text-field label="Password" v-model="password" outlined dense hide-details/>
+                </v-col>
+                <v-col cols="9">
+                  <canvas ref="canvas" id="drawing-pad" class="white"></canvas>
+                </v-col>
+                <v-col cols="5">
+                <v-btn small outlined @click="saveImage">S'inscrire</v-btn>
+              </v-col><v-col cols="5">
+                <v-btn small outlined @click="resetCanvas">Annuler</v-btn>
+              </v-col>
+              </v-row>
+            </v-list-item-content>
+          </v-list-item>
         </v-list>
       </v-navigation-drawer>
+      <img ref="img" src="" id="canvas-img">
       <nuxt />
     </v-content>
   </v-app>
@@ -54,7 +75,79 @@
         password: '',
         isLogged: false,
         logInToggle: false,
+        singIn:true,
+        canvas: null,
+        context: null,
+        isDrawing: false,
+        startX: 0,
+        startY: 0,
+        points: [],
       }
     },
+    mounted() {
+
+      this.canvas = this.$refs.canvas;
+      this.context = this.canvas.getContext('2d');
+      this.canvas.addEventListener('mousedown',this.mouseDown);
+      this.canvas.addEventListener('mousemove',this.mouseMove);
+      document.addEventListener('mouseup',this.mouseUp);
+    },
+    methods: {
+      mouseDown(e) {
+        let rect = this.canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        this.isDrawing = true;
+        this.startX = x;
+        this.startY = y;
+        this.points.push({
+          x: x,
+          y: y,
+        });
+      },
+
+      mouseMove(e) {
+        let rect = this.canvas.getBoundingClientRect();
+        let x = e.clientX - rect.left;
+        let y = e.clientY - rect.top;
+
+        if (this.isDrawing){
+          this.context.beginPath();
+          this.context.moveTo(this.startX,this.startY);
+          this.context.lineTo(x, y);
+          this.context.lineWidth = 1;
+          this.context.lineCap = 'round';
+          this.context.strokeStyle = "rgba(0,0,0,1)";
+          this.context.stroke();
+
+          this.startX = x;
+          this.startY = y;
+
+          this.points.push({
+            x: x,
+            y: y,
+          });
+        }
+      },
+
+      mouseUp() {
+        this.isDrawing = false;
+        if (this.points.length > 0 ) {
+          localStorage['points'] = JSON.stringify(this.points);
+        }
+      },
+
+      resetCanvas(){
+        this.canvas.width = this.$refs.canvas.width;
+       this.points.length = 0
+      },
+
+      saveImage(){
+        let dataUrl = this.canvas.toDataURL();
+        let img = this.$refs.img;
+        img.src = dataUrl;
+      }
+    }
   }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,7 @@
 export default {
   components: {},
   data () {
-    return {
-      tab: null,
-      text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      tabs:{
-        1:{
-          name: 'inscription'
-        },
-        2:{
-          name: 'connexion'
-        },
-      },
-    }
+    return {}
   },
 }
 </script>
